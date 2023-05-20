bb42cd1a0e55fffa71c3c9497d0185b693b2b04c
blackmoney069 <2019ucs0093@iitjammu.ac.in>
2023-05-20 13:32:45 +0530

    certificates and SSL socket created for authority

---
+++ authorityServer.py
@@ -0,0 +1,45 @@
+import socket
+import ssl
+import core.authorityRoles as authorityRoles
+
+try:
+    normal_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print ("Socket successfully created")
+except socket.error as err:
+    print ("socket creation failed with error %s" %(err))
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain(certfile='./certificates/authCert.crt', keyfile="./certificates/server.key")
+
+
+# create an instance of authority
+auth = authorityRoles.Authority()
+
+
+normal_socket.bind(("",3002))
+normal_socket.listen(5)
+ssl_socket = ssl_context.wrap_socket(normal_socket, server_side=True)
+# now the socket will listen to calls from the network
+print("Authority is listening securely at port 3002")
+
+while True:
+    conn, addr = ssl_socket.accept()
+    print("Got a connection from", addr)
+
+    # Authority sends public data and welcome message to user
+    conn.send(bytes("---------------------------------",'utf-8'))
+    conn.send(bytes("Welcome to Central Authority",'utf-8'))
+    conn.send(bytes("---------------------------------\n",'utf-8'))
+    conn.send(bytes("Public Parameters || M = {} || ".format(auth.send_MPK()),'utf-8'))
+    conn.send(bytes("Please enter your identity",'utf-8'))
+    identity = conn.recv(1024).decode('utf-8')
+    
+    # identity recieved as string from the client
+    conn.send(bytes("---- Below is the secret key for your use || KEEP IT SAFE ----",'utf-8'))
+    secret_key  = auth.keyGeneration(identity_string=identity)
+    print(secret_key)
+    conn.send(bytes(str(secret_key),'utf-8'))
+
+    conn.close()
+
+ssl_socket.close()
---
+++ core/authorityRoles.py
@@ -0,0 +1,79 @@
+import random
+import core.tools as tools
+import json
+
+def is_prime(n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+class Authority:
+    def __init__(self):
+        '''
+        Function to setup the authority and define the public and private variables
+        '''
+        self._P = None
+        self._Q = None
+        self.MPK = None
+        while(True):
+            p = random.randint(1000000,9999999)
+            q = random.randint(1000000,9999999)
+            if(p%4==3 and q%4==3) and (is_prime(n=p) and is_prime(n=q)):
+                break
+        self._P = p
+        self._Q = q
+        self.MPK = p*q
+        # print("P- ",p)
+        # print("Q- ", q)
+
+    
+    def send_MPK(self):
+        '''
+        Return the value of M to anyone accessing the Authority
+        '''
+        return self.MPK
+
+    
+    def keyGeneration(self, identity_string):
+        '''
+        This will calculate the value of a such that the jacobi of a is 1 and a represents the identity string
+
+        Parameters:
+            identity_string (string): provided to the authority to generate a private key for the user
+
+        Returns:
+            r (int): secret key for the user
+        '''
+        a = tools.hashIdentity(identity_string, self.MPK)%self.MPK
+        r = pow(a,(self.MPK+5-self._P-self._Q)//8, self.MPK)
+        return r
+    
+
+def rootVerification(MPK, r, identity_string):
+        '''
+        This is the function that will verify if the roots provided to the user are correct
+
+        Parameters:
+            MPK (int): the master public key of the authority
+            r (int) : the secret root provided to the user
+            identity_string (string) : a string that contains the identity of the user
+
+        Returns:
+            boolean if the roots provided are correct
+        '''
+        a = tools.hashIdentity(identity_string, MPK)
+        if(r**2%MPK == a%MPK):
+            print("positive correct")
+        elif(r**2%MPK == -a%MPK):
+            print("negative correct")
+        else:
+            print("Not correct") 
+
+if __name__ == "__main__":
+    auth = Authority()
+    MPK = auth.send_MPK()
+    r = auth.keyGeneration('bains')
+    rootVerification(MPK, r, identity_string='bains')
