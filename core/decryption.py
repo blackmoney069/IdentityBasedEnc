360f66352285631c4f303b33e84816ac320189ad
blackmoney069 <2019ucs0093@iitjammu.ac.in>
2023-05-23 17:43:33 +0530

    encryption decryption algorithms

---
+++ core/decryption.py
@@ -0,0 +1,63 @@
+import tools
+
+
+def decryption(s,r, ID, MPK):
+    '''
+    A function to decrypt a ciphertext.
+
+    Parameter:
+    s (pair<int>) : cipher text to be decrypted
+    r (int) : secret key of the reciever
+    ID (int) : the hash of the identity of the reciever
+    MPK (int) : master public key of the authority
+
+    Returns:
+    M (bit): one or zero decrypted bit
+    '''
+
+    if r**2%MPK == ID%MPK:
+        alpha = s[0] + 2*r
+    else:
+        alpha = s[1] + 2*r
+    return tools.jacobi_symbol(alpha, MPK)
+
+
+def decrypt_sequence(s,r, ID, MPK):
+    '''
+    A function to decrypt a ciphertext.
+
+    Parameter:
+    s (list of pair<int>) : list of cipher texts to be decrypted
+    r (int) : secret key of the reciever
+    ID (int) : the hash of the identity of the reciever
+    MPK (int) : master public key of the authority
+
+    Returns:
+    M (string): string of one or zero decrypted bits
+    '''
+    output = ""
+    for i in s:
+        if r**2%MPK == ID%MPK:
+            alpha = i[0] + 2*r
+        else:
+            alpha = i[1] + 2*r
+        res = tools.jacobi_symbol(alpha, MPK)
+        if(res==1):
+            output+="1"
+        else:
+            output+="0"
+    
+    return output
+
+
+
+if __name__ =="__main__":
+
+    f = open("testcase.json")
+    #test case included in json format
+
+    input = eval("[(53199093286327, 3771170403324), (40924870912374, 16045392377559), (18703193682098, 38267069731397)]")
+     
+    first_bit = decrypt_sequence(input,38041879909636, 28877774692113, 56970261955253)
+    # first_bit = decrypt_sequence(input,test_case["params"]["reciever"]['r'], test_case["params"]["reciever"]['ID'], test_case["params"]["authority"]['MPK'])
+    print(first_bit)
---
+++ core/encryptionScheme.py
@@ -0,0 +1,92 @@
+import tools
+import random
+
+def mod_inverse(a, m):
+    """
+    Calculates the modular inverse of a modulo m.
+
+    Parameters:
+    a (int): The number to calculate the inverse of.
+    m (int): The modulus.
+
+    Returns:
+    The modular inverse of a modulo m, or None if it does not exist.
+    """
+    # Check if gcd(a, m) = 1, otherwise the inverse does not exist
+    gcd, x, y = extended_euclidean_algorithm(a, m)
+    if gcd != 1:
+        return None
+    
+    # Calculate the inverse using the extended Euclidean algorithm
+    return (x % m + m) % m
+
+
+def extended_euclidean_algorithm(a, b):
+    """
+    Calculates the greatest common divisor (gcd) of a and b, as well as the
+    Bezout coefficients x and y such that ax + by = gcd(a, b).
+
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+
+    Returns:
+    A tuple (gcd, x, y) such that ax + by = gcd(a, b).
+    """
+    # Base case
+    if a == 0:
+        return (b, 0, 1)
+    
+    # Recursive call
+    gcd, x1, y1 = extended_euclidean_algorithm(b % a, a)
+    
+    # Calculate the Bezout coefficients
+    x = y1 - (b // a) * x1
+    y = x1
+    
+    return (gcd, x, y)
+
+
+def encrypt(string, MPK, ID):
+    '''
+    This function will return the encrypted array of tuples
+
+    Parameter:
+    string (string) : the bianry string to be encrypted (will be used as a key).
+    MPK (int) :  Master public key of the authority.
+    ID (int) : hashed identity of the reciever .
+
+    Returns:
+    a list of tuples that have the c1,c2 pair of each bit
+    '''
+    output=[]
+    for i in string:
+        t1= None
+        t2 = None
+        if(i=="1"):
+            while(True):
+                t1 = random.randint(0,999999)
+                jacobi1 = tools.jacobi_symbol(t1,MPK)
+                t2 = random.randint(0,999999)
+                jacobi2 = tools.jacobi_symbol(t2,MPK)
+                if(jacobi1==1 and jacobi2==1 and t1!=t2):
+                    break
+        else:
+            while(True):
+                t1 = random.randint(0,999999)
+                jacobi1 = tools.jacobi_symbol(t1,MPK)
+                t2 = random.randint(0,999999)
+                jacobi2 = tools.jacobi_symbol(t2,MPK)
+                if(jacobi1==-1 and jacobi2==-1 and t1!=t2):
+                    break
+        
+        c1 = (t1 + ID * mod_inverse(t1,MPK) )%MPK
+        c2 = (t2 - ID * mod_inverse(t1,MPK) )%MPK
+        output.append((c1,c2))
+    return output
+
+if __name__ =="__main__":
+    f = open("testcase.json")
+    # test_case = json.load(f)
+    # print(encrypt(input_string, test_case['params']['authority']["MPK"], test_case['params']['reciever']["ID"] )) 
+    print(encrypt('101', 56970261955253, 28877774692113)) 
